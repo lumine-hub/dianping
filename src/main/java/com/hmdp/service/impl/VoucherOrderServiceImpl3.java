b1e043f138eecbf9d63e611bd6bac90bf77278e0
xuluming <2761151386@qq.com>
2024-06-13 22:06:01 +0800

    debug

--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl3.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl3.java
@@ -1,153 +1,153 @@
-package com.hmdp.service.impl;
-
-import com.alibaba.fastjson.JSON;
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import com.hmdp.dto.Result;
-import com.hmdp.entity.VoucherOrder;
-import com.hmdp.mapper.VoucherOrderMapper;
-import com.hmdp.service.ISeckillVoucherService;
-import com.hmdp.service.IVoucherOrderService;
-import com.hmdp.service.IVoucherService;
-import com.hmdp.utils.RedisIdWorker;
-import com.hmdp.utils.SeckillConstants;
-import com.hmdp.utils.UserHolder;
-import org.apache.commons.lang3.StringUtils;
-import org.redisson.api.RLock;
-import org.redisson.api.RedissonClient;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.core.script.DefaultRedisScript;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import javax.annotation.PostConstruct;
-import javax.annotation.Resource;
-import java.util.Collections;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
-/**
- * <p>
- *  服务实现类（采用lua+阻塞队列)
- * </p>
- *
- * @author xlm
- * @since 2024-6-13
- */
-@Service
-public class VoucherOrderServiceImpl3 extends ServiceImpl<VoucherOrderMapper, VoucherOrder> implements IVoucherOrderService {
-
-    @Autowired
-    private IVoucherService voucherService;
-    @Autowired
-    private ISeckillVoucherService seckillVoucherService;
-
-    @Resource
-    private StringRedisTemplate stringRedisTemplate;
-
-    //异步处理线程池
-    @Autowired
-    private RedissonClient redissonClient;
-
-    @Autowired
-    private KafkaTemplate kafkaTemplate;
-
-    private static final DefaultRedisScript<Long> SECKILL_SCRIPT;
-
-    static {
-        SECKILL_SCRIPT = new DefaultRedisScript<>();
-        SECKILL_SCRIPT.setLocation(new ClassPathResource("seckill.lua"));
-        SECKILL_SCRIPT.setResultType(Long.class);
-    }
-
-    //使用lua脚本执行是否有购买资格，之后创建订单给消息队列
-    @Override
-    public Result seckillVoucher(Long voucherId) {
-        Long userId = UserHolder.getUser().getId();
-        // 1.执行lua脚本
-        Long result = stringRedisTemplate.execute(
-                SECKILL_SCRIPT,
-                Collections.emptyList(),
-                voucherId.toString(), userId.toString()
-        );
-        int r = result.intValue();
-        // 2.判断结果是否为0
-        if (r != 0) {
-            // 2.1.不为0 ，代表没有购买资格
-            return Result.fail(r == 1 ? "库存不足" : "不能重复下单");
-        }
-
-        // 2.1 有购买资格，需要把信息保存到阻塞队列。
-        VoucherOrder voucherOrder = new VoucherOrder();
-        long orderId = RedisIdWorker.nextId("order");
-        voucherOrder.setId(orderId);
-        // 2.2.用户id
-        voucherOrder.setUserId(userId);
-        // 2.3.代金券id
-        voucherOrder.setVoucherId(voucherId);
-        // 2.4放到消息队列
-        kafkaTemplate.send(SeckillConstants.SECKILL_ORDER_INFO_TOPIC, JSON.toJSONString(voucherOrder));
-        // 3.返回订单id
-        return Result.ok(orderId);
-    }
-
-
-    @KafkaListener(topics = SeckillConstants.SECKILL_ORDER_INFO_TOPIC)
-    public void onMessage(String message) {
-        if (StringUtils.isNotBlank(message)) {
-            VoucherOrder voucherOrder = JSON.parseObject(message, VoucherOrder.class);
-            handleVoucherOrder(voucherOrder);
-        }
-    }
-    private void handleVoucherOrder(VoucherOrder voucherOrder) {
-        //1.获取用户
-        Long userId = voucherOrder.getUserId();
-        // 2.创建锁对象
-        RLock redisLock = redissonClient.getLock("lock:order:" + userId);
-        // 3.尝试获取锁
-        boolean isLock = redisLock.tryLock();
-        // 4.判断是否获得锁成功
-        if (!isLock) {
-            // 获取锁失败，直接返回失败或者重试
-            log.error("不允许重复下单！");
-            return;
-        }
-        try {
-            //注意：由于是spring的事务是放在threadLocal中，此时的是多线程，事务会失效
-            createVoucherOrder(voucherOrder);
-        } finally {
-            // 释放锁
-            redisLock.unlock();
-        }
-    }
-    @Transactional
-    public void createVoucherOrder(VoucherOrder voucherOrder) {
-        Long userId = voucherOrder.getUserId();
-        // 5.1.查询订单
-        int count = query().eq("user_id", userId).eq("voucher_id", voucherOrder.getVoucherId()).count();
-        // 5.2.判断是否存在
-        if (count > 0) {
-            // 用户已经购买过了
-            log.error("用户已经购买过了");
-            return;
-        }
-
-        // 6.扣减库存
-        boolean success = seckillVoucherService.update()
-                .setSql("stock = stock - 1") // set stock = stock - 1
-                .eq("voucher_id", voucherOrder.getVoucherId()).gt("stock", 0) // where id = ? and stock > 0
-                .update();
-        if (!success) {
-            // 扣减失败
-            log.error("库存不足");
-            return;
-        }
-        save(voucherOrder);
-
-    }
-}
+//package com.hmdp.service.impl;
+//
+//import com.alibaba.fastjson.JSON;
+//import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+//import com.hmdp.dto.Result;
+//import com.hmdp.entity.VoucherOrder;
+//import com.hmdp.mapper.VoucherOrderMapper;
+//import com.hmdp.service.ISeckillVoucherService;
+//import com.hmdp.service.IVoucherOrderService;
+//import com.hmdp.service.IVoucherService;
+//import com.hmdp.utils.RedisIdWorker;
+//import com.hmdp.utils.SeckillConstants;
+//import com.hmdp.utils.UserHolder;
+//import org.apache.commons.lang3.StringUtils;
+//import org.redisson.api.RLock;
+//import org.redisson.api.RedissonClient;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.core.io.ClassPathResource;
+//import org.springframework.data.redis.core.StringRedisTemplate;
+//import org.springframework.data.redis.core.script.DefaultRedisScript;
+//import org.springframework.kafka.annotation.KafkaListener;
+//import org.springframework.kafka.core.KafkaTemplate;
+//import org.springframework.stereotype.Service;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//import javax.annotation.PostConstruct;
+//import javax.annotation.Resource;
+//import java.util.Collections;
+//import java.util.concurrent.ArrayBlockingQueue;
+//import java.util.concurrent.BlockingQueue;
+//import java.util.concurrent.ExecutorService;
+//import java.util.concurrent.Executors;
+//
+///**
+// * <p>
+// *  服务实现类（采用lua+阻塞队列)
+// * </p>
+// *
+// * @author xlm
+// * @since 2024-6-13
+// */
+////@Service
+//public class VoucherOrderServiceImpl3 extends ServiceImpl<VoucherOrderMapper, VoucherOrder> implements IVoucherOrderService {
+//
+//    @Autowired
+//    private IVoucherService voucherService;
+//    @Autowired
+//    private ISeckillVoucherService seckillVoucherService;
+//
+//    @Resource
+//    private StringRedisTemplate stringRedisTemplate;
+//
+//    //异步处理线程池
+//    @Autowired
+//    private RedissonClient redissonClient;
+//
+//    @Autowired
+//    private KafkaTemplate kafkaTemplate;
+//
+//    private static final DefaultRedisScript<Long> SECKILL_SCRIPT;
+//
+//    static {
+//        SECKILL_SCRIPT = new DefaultRedisScript<>();
+//        SECKILL_SCRIPT.setLocation(new ClassPathResource("seckill.lua"));
+//        SECKILL_SCRIPT.setResultType(Long.class);
+//    }
+//
+//    //使用lua脚本执行是否有购买资格，之后创建订单给消息队列
+//    @Override
+//    public Result seckillVoucher(Long voucherId) {
+//        Long userId = UserHolder.getUser().getId();
+//        // 1.执行lua脚本
+//        Long result = stringRedisTemplate.execute(
+//                SECKILL_SCRIPT,
+//                Collections.emptyList(),
+//                voucherId.toString(), userId.toString()
+//        );
+//        int r = result.intValue();
+//        // 2.判断结果是否为0
+//        if (r != 0) {
+//            // 2.1.不为0 ，代表没有购买资格
+//            return Result.fail(r == 1 ? "库存不足" : "不能重复下单");
+//        }
+//
+//        // 2.1 有购买资格，需要把信息保存到阻塞队列。
+//        VoucherOrder voucherOrder = new VoucherOrder();
+//        long orderId = RedisIdWorker.nextId("order");
+//        voucherOrder.setId(orderId);
+//        // 2.2.用户id
+//        voucherOrder.setUserId(userId);
+//        // 2.3.代金券id
+//        voucherOrder.setVoucherId(voucherId);
+//        // 2.4放到消息队列
+//        kafkaTemplate.send(SeckillConstants.SECKILL_ORDER_INFO_TOPIC, JSON.toJSONString(voucherOrder));
+//        // 3.返回订单id
+//        return Result.ok(orderId);
+//    }
+//
+//
+//    @KafkaListener(topics = SeckillConstants.SECKILL_ORDER_INFO_TOPIC)
+//    public void onMessage(String message) {
+//        if (StringUtils.isNotBlank(message)) {
+//            VoucherOrder voucherOrder = JSON.parseObject(message, VoucherOrder.class);
+//            handleVoucherOrder(voucherOrder);
+//        }
+//    }
+//    private void handleVoucherOrder(VoucherOrder voucherOrder) {
+//        //1.获取用户
+//        Long userId = voucherOrder.getUserId();
+//        // 2.创建锁对象
+//        RLock redisLock = redissonClient.getLock("lock:order:" + userId);
+//        // 3.尝试获取锁
+//        boolean isLock = redisLock.tryLock();
+//        // 4.判断是否获得锁成功
+//        if (!isLock) {
+//            // 获取锁失败，直接返回失败或者重试
+//            log.error("不允许重复下单！");
+//            return;
+//        }
+//        try {
+//            //注意：由于是spring的事务是放在threadLocal中，此时的是多线程，事务会失效
+//            createVoucherOrder(voucherOrder);
+//        } finally {
+//            // 释放锁
+//            redisLock.unlock();
+//        }
+//    }
+//    @Transactional
+//    public void createVoucherOrder(VoucherOrder voucherOrder) {
+//        Long userId = voucherOrder.getUserId();
+//        // 5.1.查询订单
+//        int count = query().eq("user_id", userId).eq("voucher_id", voucherOrder.getVoucherId()).count();
+//        // 5.2.判断是否存在
+//        if (count > 0) {
+//            // 用户已经购买过了
+//            log.error("用户已经购买过了");
+//            return;
+//        }
+//
+//        // 6.扣减库存
+//        boolean success = seckillVoucherService.update()
+//                .setSql("stock = stock - 1") // set stock = stock - 1
+//                .eq("voucher_id", voucherOrder.getVoucherId()).gt("stock", 0) // where id = ? and stock > 0
+//                .update();
+//        if (!success) {
+//            // 扣减失败
+//            log.error("库存不足");
+//            return;
+//        }
+//        save(voucherOrder);
+//
+//    }
+//}
