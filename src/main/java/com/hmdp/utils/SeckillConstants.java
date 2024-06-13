818875c4604f440f49c179f9eb804f71a199213f
xuluming <2761151386@qq.com>
2024-06-13 21:55:44 +0800

    kafka消息队列优化秒杀

--- src/main/java/com/hmdp/utils/SeckillConstants.java
+++ src/main/java/com/hmdp/utils/SeckillConstants.java
@@ -3,4 +3,5 @@ package com.hmdp.utils;
 public class SeckillConstants {
     public static final String SECKILL_STOCK_KEY = "seckill:stock:";
     public static final String SECKILL_ORDER_KEY = "seckill:order:";
+    public static final String SECKILL_ORDER_INFO_TOPIC = "seckill_order_info_topic";
 }
